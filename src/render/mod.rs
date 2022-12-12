95d223aa1953e6e357b37bd2c5d319dabb47cd50
Dave Ceddia <dave@daveceddia.com>
2022-12-12 01:34:06 -0500

    Got video to render to the screen! Needed to set the sampler variable locations with glUniform, and also needed to use sampler2DRect in the fragment shader (and GL_TEXTURE_RECTANGLE_ARB everywhere instead of GL_TEXTURE_2D because the IOSurface function only works in RECTANGLE mode)

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,11 +1,16 @@
 use crate::{codec::video::VideoFrame, Error};
-use std::os::raw::{c_int, c_uint, c_void};
+use std::os::raw::{c_int, c_void};
 
 #[cfg(target_os = "macos")]
 #[link(name = "IOSurface", kind = "framework")]
 extern "C" {
     fn gl_renderer_new() -> *mut c_void;
-    fn gl_renderer_render(renderer: *mut c_void, cv_pixel_buffer_ref: *mut c_void) -> c_int;
+    fn gl_renderer_render(
+        renderer: *mut c_void,
+        cv_pixel_buffer_ref: *mut c_void,
+        sampler_y_location: c_int,
+        sampler_uv_location: c_int,
+    ) -> c_int;
     fn gl_renderer_free(renderer: *mut c_void);
 }
 
@@ -25,10 +30,22 @@ impl GlRenderer {
         GlRenderer { ptr }
     }
 
-    pub fn render(&self, frame: &VideoFrame) -> Result<(), Error> {
+    pub fn render(
+        &self,
+        frame: &VideoFrame,
+        sampler_y_location: u32,
+        sampler_uv_location: u32,
+    ) -> Result<(), Error> {
         let pixel_buffer = frame.planes()[3].data().as_ptr();
         println!("GlRenderer::render pixel_buffer {:p}", pixel_buffer);
-        let res = unsafe { gl_renderer_render(self.ptr, pixel_buffer as _) };
+        let res = unsafe {
+            gl_renderer_render(
+                self.ptr,
+                pixel_buffer as _,
+                sampler_y_location as _,
+                sampler_uv_location as _,
+            )
+        };
 
         if res != 0 {
             Err(Error::new(format!("failed to render frame")))
